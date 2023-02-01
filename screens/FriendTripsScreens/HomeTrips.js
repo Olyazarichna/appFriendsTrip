4b1fcffc5189523d22c10430ce70c9914bc4cd8b
Serhii Rybak <rybaksw@gmail.com>
2023-02-01 17:36:11 +0200

    fix: import; style: search bar

--- screens/FriendTripsScreens/HomeTrips.js
+++ screens/FriendTripsScreens/HomeTrips.js
@@ -1,25 +1,24 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TextInput,
-  TouchableOpacity,
-  Platform,
-  KeyboardAvoidingView,
-  TouchableWithoutFeedback,
-  Keyboard,
-  ImageBackground,
-  SafeAreaView,
-  Pressable,
-} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
+import Constants from 'expo-constants';
+import { Ionicons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import ListTrip from '../../components/ListTrip/ListTrip';
 
 import { tripData } from '../../hardcodedData/tripData';
+import FilterIcon from '../../components/FilterIcon/FilterIcon';
 
 export default function HomeTrips() {
   const intervals = tripData.length;
+
+  const handleFilterPress = () => {
+    console.log('Open filter settings.');
+  };
+
+  const handleSearchPress = () => {
+    console.log('Send search query.');
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -27,10 +26,26 @@ export default function HomeTrips() {
         <Text style={styles.title}>TRVL</Text>
       </View>
       <View style={styles.search}>
-        <TextInput style={styles.searchInput} placeholder="Search Trip.." />
-        <Pressable style={styles.button}>
-          <Text>Filter</Text>
-        </Pressable>
+        <View style={styles.searchContainer}>
+          <Pressable style={styles.searchIcon} onPress={handleSearchPress}>
+            <Ionicons
+              name="ios-search"
+              size={20}
+              color={'rgba(28, 79, 165, 0.07)'}
+            />
+          </Pressable>
+          <TextInput style={styles.searchInput} placeholder="Search Trip.." />
+        </View>
+        <LinearGradient
+          style={styles.button}
+          start={{ x: 0.5, y: 0.5 }}
+          end={{ x: 0.5, y: 0.5 }}
+          colors={['rgb(69, 124, 247)', 'rgb(55, 90, 190)']}
+        >
+          <Pressable style={styles.button} onPress={handleFilterPress}>
+            <FilterIcon />
+          </Pressable>
+        </LinearGradient>
       </View>
       <View style={styles.list}>
         <ListTrip intervals={intervals} trips={tripData} />
@@ -48,7 +63,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingTop: 60,
+    marginTop: 14 + Constants.statusBarHeight,
     paddingHorizontal: 24,
   },
   title: {
@@ -61,13 +76,22 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     paddingHorizontal: 25,
   },
+  searchContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    marginRight: 16,
+  },
+  searchIcon: {
+    paddingLeft: 24,
+    paddingRight: 20,
+  },
   searchInput: {
     flex: 1,
     height: 64,
-    marginRight: 16,
-    padding: 10,
-    borderRadius: 20,
-    backgroundColor: '#fff',
     fontSize: 14,
   },
   button: {
@@ -76,7 +100,7 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     height: 64,
     width: 64,
-    backgroundColor: 'royalblue',
+    // backgroundColor: 'transparent',
     // backgroundColor: 'linear-gradient(95.23deg, #457CF7 0%, #375ABE 100%)',
   },
   list: {
